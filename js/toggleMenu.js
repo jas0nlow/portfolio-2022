4c314666ef1da792ec56d034300aa52188a5c981
bachlava <bachlava@proton.me>
2025-10-19 23:36:16 -0700

    feat: add footer, add on-active button animations, update tablet/mobile hamburger menu with close icon and expanded nav menu now  covers the entire view

--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -1,8 +1,11 @@
 function toggleMenu() {
-  var navLinks = document.getElementById("nav-links");
+  let navLinks = document.getElementById("nav-links");
+  let hmbgrIcon = document.getElementById("hmbgr-menu");
   if (navLinks.style.display === "block") {
     navLinks.style.display = "none";
+    hmbgrIcon.innerHTML = '<i class="fa fa-bars"></i>';
   } else {
     navLinks.style.display = "block";
+    hmbgrIcon.innerHTML = '<i class="fa fa-close"></i>';
   }
 } 
