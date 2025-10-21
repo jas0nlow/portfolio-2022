de9cdef2c8612c213f655abf48151f48566d961b
bachlava <bachlava@proton.me>
2025-10-21 01:41:20 -0700

    feat: add star on hover for header bar's logo and nav item(s), add expand anim for hmbrg menu, fix intro's contact btn oversized click area

--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -1,11 +1,11 @@
 function toggleMenu() {
   let navLinks = document.getElementById("nav-links");
   let hmbgrIcon = document.getElementById("hmbgr-menu");
-  if (navLinks.style.display === "block") {
-    navLinks.style.display = "none";
+  if (navLinks.className === "nav-links menu-open") {
+    navLinks.className = "nav-links menu-closed";
     hmbgrIcon.innerHTML = '<i class="fa fa-bars"></i>';
   } else {
-    navLinks.style.display = "block";
+    navLinks.className = "nav-links menu-open";
     hmbgrIcon.innerHTML = '<i class="fa fa-close"></i>';
   }
 } 
